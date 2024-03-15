e9647b1db9e0d38780f5679ea3be6fd807603696
Kirti Vardhan <kirtivardhan8@gmail.com>
2024-03-15 20:51:51 +0530

    Project created

---
+++ src/main/java/com/thirdparty/apiservice/YobThirdpartyApiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.thirdparty.apiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YobThirdpartyApiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YobThirdpartyApiServiceApplication.class, args);
+	}
+
+}
